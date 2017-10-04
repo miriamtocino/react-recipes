4541686e38ca09d86a9c9f9943f7c4e6c319feb0
Miriam Tocino <miriam.tocino@gmail.com>
2017-10-04 15:30:25 +0200

    The Reducer

---
+++ src/reducers/recipes.test.js
@@ -0,0 +1,11 @@
+import chai, { expect } from 'chai'
+import recipes from './recipes'
+
+describe('recipes reducer', () => {
+  const reducer = recipes
+  const initialState = []
+
+  it('returns an empty array for the initial state', () => {
+    expect(reducer()).to.eql(initialState)
+  })
+})
